e42adc0b04b26d19798cc580eeee51418902ebff
Inna Fedorova <innamassura@gmail.com>
2025-07-13 16:59:43 +0300

    add: component header

---
+++ src/components/Header.tsx
@@ -0,0 +1,26 @@
+import { Component, createRef } from 'react';
+import type { HeaderProps } from '@types';
+
+class Header extends Component<HeaderProps> {
+    inputRef = createRef<HTMLInputElement>();
+
+    handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        localStorage.setItem('value', this.inputRef.current?.value || '')
+    }
+  render() {
+    return (
+      <header className="header">
+        <form onSubmit={this.handleSubmit}>
+            <label>
+                <p className='label-search'>Enter country name</p>
+                <input type="text" className="input-search" defaultValue={localStorage.getItem('value') || ''} ref={this.inputRef}/>
+            </label>
+            <button type="submit" className="button-search">Search</button>
+        </form>
+      </header>
+    );
+  }
+}
+
+export default Header;
